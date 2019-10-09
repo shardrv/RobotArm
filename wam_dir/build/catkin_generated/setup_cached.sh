f2d50efcecec0b9d703dbe8dd78c47fa8fb6024d
shardrv <dsharma@ualberta.ca>
2019-10-09 15:14:17 -0600

    ROS Node

--- wam_dir/build/catkin_generated/setup_cached.sh
+++ wam_dir/build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/dhruv/RobotArm/wam_dir/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/dhruv/RobotArm/wam_dir/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/dhruv/RobotArm/wam_dir/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/dhruv/RobotArm/wam_dir/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/dhruv/RobotArm/wam_dir/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/dhruv/RobotArm/wam_dir/src:$ROS_PACKAGE_PATH"

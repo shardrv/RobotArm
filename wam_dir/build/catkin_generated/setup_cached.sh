d905c85f95cbd3daba18de30bef208b5fb450602
shardrv <dsharma@ualberta.ca>
2019-10-09 13:16:04 -0600

    Setup corrected

---
+++ wam_dir/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/dhruv/RobotArm/wam_dir/devel:/opt/ros/kinetic"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu"
+export PWD="/home/dhruv/RobotArm/wam_dir/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/dhruv/RobotArm/wam_dir/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/dhruv/RobotArm/wam_dir/src:/opt/ros/kinetic/share"
--- wamdir/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dhruv/RobotArm/wamdir/src
-# Build directory: /home/dhruv/RobotArm/wamdir/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
